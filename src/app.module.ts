0d0fc832e9f704c54e1751e98e1e7f561fb9d671
Ahmed <ahmdhusam7@gmail.com>
2022-07-26 20:05:27 +0200

    configure file structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { CartModule } from './cart/cart.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
@@ -18,11 +21,16 @@ import { AppService } from './app.service';
         password: config.get('DBPASSWORD'),
         database: 'nest',
         entities: [],
-        synchronize: config.get('NODE_ENV') === 'PRODUCTION',
+        synchronize: config.get('NODE_ENV') !== 'production',
       }),
     }),
+    UsersModule,
+    AuthModule,
+    ProductsModule,
+    CartModule,
+    OrdersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
