f23f5ed58eaed615f34ae1cfc646adeb4d50fedc
Ahmed <ahmdhusam7@gmail.com>
2022-08-18 13:44:24 +0200

    fix issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Cart } from './cart/cart.entity';
+import { User } from './users/users.entity';
+import { Product } from './products/products.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
 import { CartModule } from './cart/cart.module';
 import { OrdersModule } from './orders/orders.module';
-import { User } from './users/users.entity';
-import { Product } from './products/products.entity';
-import { Cart } from './cart/cart.entity';
 
 @Module({
   imports: [
