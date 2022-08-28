b35ce378bb842a5d8e11a7b6670f8877e161ae7c
Ahmed <ahmdhusam7@gmail.com>
2022-08-28 14:23:44 +0200

    add users/:username endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UserSerialize } from 'src/auth/dtos';
+import { UseAuthGuard } from 'src/auth/guards';
 import { UseSerialize } from 'src/interceptors/serialize.interceptor';
 import { CurrentUser } from './decorators';
 import { User } from './users.entity';
+import { UsersService } from './users.service';
 
 @UseSerialize(UserSerialize)
+@UseAuthGuard()
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get('me')
   me(@CurrentUser() currentUser: User): UserSerialize {
     return currentUser;
+  }
+
+  @Get(':username')
+  getUser(@Param('username') username: string): Promise<UserSerialize> {
+    return this.usersService.getUserBy({ username });
   }
 }
