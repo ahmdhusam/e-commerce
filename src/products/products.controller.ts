08145de55ed815c70b9d6c207b1bf97713364700
Ahmed <ahmdhusam7@gmail.com>
2022-08-29 18:00:19 +0200

    add products/update-product endpoint

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Put } from '@nestjs/common';
 import { UseAuthGuard } from 'src/auth/guards';
 import { UseSerialize } from 'src/interceptors/serialize.interceptor';
 import { CurrentUser } from 'src/users/decorators';
 import { User } from 'src/users/users.entity';
 import { ProductData, ProductSerialize } from './dtos';
+import { UpdateProduct } from './dtos/update-product.dto';
 import { ProductsService } from './products.service';
 
 @UseSerialize(ProductSerialize)
@@ -15,5 +16,13 @@ export class ProductsController {
   @Post('create-product')
   createProduct(@CurrentUser() currentUser: User, @Body() productData: ProductData): Promise<ProductSerialize> {
     return this.productsService.create(currentUser, productData);
+  }
+
+  @Put('update-product')
+  updateProduct(
+    @CurrentUser() currentUser: User,
+    @Body() { id, ...productData }: UpdateProduct,
+  ): Promise<ProductSerialize> {
+    return this.productsService.update(id, productData, currentUser);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/users.entity';
 import { Repository } from 'typeorm';
 import { ProductData } from './dtos';
+import { UpdateProduct } from './dtos/update-product.dto';
 import { Product } from './products.entity';
 
 @Injectable()
@@ -11,5 +12,23 @@ export class ProductsService {
 
   create(author: User, productData: ProductData): Promise<Product> {
     return this.productsRepo.create({ ...productData, author }).save();
+  }
+
+  async update(productId: string, productData: Omit<UpdateProduct, 'id'>, currentUser: User): Promise<Product> {
+    const product = await this.getOneById(productId);
+    this.isAuthorized(currentUser, product);
+
+    Object.assign(product, productData);
+    return product.save();
+  }
+
+  isAuthorized(user: User, product: Product): void {
+    if (product.author.id !== user.id) throw new ForbiddenException('unauthorized');
+  }
+
+  getOneById(productId: string): Promise<Product> {
+    return this.productsRepo.findOneOrFail({ where: { id: productId }, relations: ['author'] }).catch(() => {
+      throw new NotFoundException('product Not Found');
+    });
   }
 }
