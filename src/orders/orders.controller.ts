7d5b023d17e1a365cd894a2657f645d566bc1c04
Ahmed <ahmdhusam7@gmail.com>
2022-09-26 18:05:15 +0200

    add checkout endpoint

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,9 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UseAuthGuard } from 'src/auth/guards';
+import { CurrentUser } from 'src/users/decorators';
+import { User } from 'src/users/users.entity';
+import { CreateOrderDto } from './dtos';
 import { OrdersService } from './orders.service';
 
 @UseAuthGuard()
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
+
+  @Post('checkout')
+  async checkout(@CurrentUser() currentUser: User, @Body() orderData: CreateOrderDto): Promise<{ message: string }> {
+    await this.ordersService.checkout(currentUser, orderData);
+    return { message: 'successful' };
+  }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,13 +1,64 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { CartService } from 'src/cart/cart.service';
-import { Repository } from 'typeorm';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { isPositive } from 'class-validator';
+import { InjectStripe } from 'nestjs-stripe';
+import { Cart } from 'src/cart/cart.entity';
+import { User } from 'src/users/users.entity';
+import Stripe from 'stripe';
+import { EntityManager } from 'typeorm';
+import { CreateOrderDto } from './dtos';
 import { Orders } from './orders.entity';
 
 @Injectable()
 export class OrdersService {
   constructor(
-    @InjectRepository(Orders) private readonly ordersRepo: Repository<Orders>,
-    private readonly cartService: CartService,
+    @InjectEntityManager() private readonly entityManager: EntityManager,
+    @InjectStripe() private readonly stripe: Stripe,
   ) {}
+
+  async checkout(user: User, orderData: CreateOrderDto): Promise<void> {
+    let { total }: { total: number } = await this.entityManager
+      .createQueryBuilder(Cart, 'cart')
+      .select('SUM(cart.quantity * product.price)', 'total')
+      .innerJoin('cart.product', 'product')
+      .where('cart.owner_id = :ownerId', { ownerId: user.id })
+      .andWhere('cart.in_order = false')
+      .getRawOne();
+
+    if (!isPositive(total)) throw new BadRequestException();
+    total = +total.toFixed(2);
+
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card: {
+        number: orderData.cardNumber,
+        cvc: orderData.cardCvc,
+        exp_month: orderData.cardExpMonth,
+        exp_year: orderData.cardExpYear,
+      },
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      amount: total * 100,
+      currency: 'usd',
+      confirm: true,
+      payment_method_types: ['card'],
+      payment_method: paymentMethod.id,
+    });
+
+    const order = Orders.create({
+      payment: paymentIntent.id,
+      city: orderData.city,
+      country: orderData.country,
+      address: orderData.address,
+      owner: user,
+      total,
+    });
+
+    await this.entityManager.transaction('SERIALIZABLE', async transactionManager => {
+      await transactionManager.save(order);
+
+      await transactionManager.update(Cart, { ownerId: user.id, inOrder: false }, { inOrder: true, order });
+    });
+  }
 }
