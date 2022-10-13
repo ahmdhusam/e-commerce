61880ebb141dc1cf23a4d45d2211a5d5032e657b
Ahmed <ahmdhusam7@gmail.com>
2022-10-13 17:13:32 +0200

    UPDATE: configure SkipAuth

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,7 +1,25 @@
-import { Injectable, UseGuards } from '@nestjs/common';
+import { ExecutionContext, Injectable, SetMetadata, UseGuards } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
+export const SKIP_AUTH_KEY = 'isPublic';
+export const SkipAuth = () => SetMetadata(SKIP_AUTH_KEY, true);
 export const UseAuthGuard = () => UseGuards(JwtAuthGuard);
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(SKIP_AUTH_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+    return super.canActivate(context);
+  }
+}
